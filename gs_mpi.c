3cb6ed0f7d2f62411a25947ff57a109f19592837
Sinclert <Sinclert@hotmail.com>
2018-10-27 14:31:28 +0200

    Substituting matrix funcs by array funcs

--- gs_mpi.c
+++ gs_mpi.c
@@ -28,43 +28,26 @@ int get_max_rows(int num_nodes, int n) {
 
 
 
-// Calculates how many rows are going to a given node
-int get_node_rows(int lower_bound, int upper_bound) {
-	return upper_bound - lower_bound + 1;
+// Gets the position from which elements are gonna be sent / received
+int get_node_offset(int node_id, int n, int max_rows) {
+	return node_id * n * (max_rows-2);
 }
 
 
 
 
 // Calculates how many elements are going to a given node
-int get_node_elems(int node_rows, int n) {
-	return node_rows * n;
-}
+int get_node_elems(int node_offset, int n, int max_rows) {
+	int node_elems = max_rows*n
 
-
-
-
-// Gets the index from which each node should get rows
-// SUPPOSITION 1: STARTS WITH NODE 0
-// SUPPOSITION 2: THE RETURNED INDEX IS INCLUDED
-int get_lower_index(int node_id, int max_rows) {
-	return node_id * (max_rows-2);
-}
-
-
-
-
-// Gets the index until which each node should get rows
-// SUPPOSITION 1: STARTS WITH NODE 0
-// SUPPOSITION 2: THE RETURNED INDEX IS INCLUDED
-int get_upper_index(int node_id, int max_rows, int n) {
-
-	int index = (node_id+1) * (max_rows-2) + 1;
-	if (index >= n) {
-		return n-1;
+	// Case in which the node receive the full set of elements
+	if (node_offset + node_elems < (n*n)) {
+		return node_elems
 	}
+
+	// Case of the last node, which could get less elements
 	else {
-		return index;
+		return (node_offset+node_elems) - (n*n)
 	}
 }
 
@@ -72,7 +55,7 @@ int get_upper_index(int node_id, int max_rows, int n) {
 
 
 // Allocate 2D matrix in the master node
-void allocate_init_2Dmatrix(float **mat, int n, int m){
+void allocate_root_matrix(float **mat, int n, int m){
 
 	*mat = (float *) malloc(n * m * sizeof(float));
 	for (int i = 0; i < (n*m); i++) {
@@ -84,8 +67,8 @@ void allocate_init_2Dmatrix(float **mat, int n, int m){
 
 
 // Allocate 2D matrix in the slaves nodes
-void allocate_nodes_2Dmatrix(float **mat, int n, int m) {
-	*mat = (float *) malloc(n * m * sizeof(float));
+void allocate_node_matrix(float **mat, int num_elems) {
+	*mat = (float *) malloc(num_elems * sizeof(float));
 }
 
 
@@ -170,13 +153,10 @@ int main(int argc, char *argv[]) {
 	printf("Matrix size = %d communication = %d\n", n, communication);
 
 
-	int max_rows = get_max_rows(np, n);
-
 	// Calculate common relevant values for each node
-	int lower_index = get_lower_index(myrank, max_rows);
-	int upper_index = get_upper_index(myrank, max_rows, n);
-	int num_rows = get_node_rows(lower_index, upper_index);
-	int num_elems = get_node_elems(num_rows, n);
+	int max_rows = get_max_rows(np, n);
+	int offset = get_node_offset(myrank, n, max_rows);
+	int elems = get_node_elems(myrank, n, max_rows);
 
 	double tscom1 = MPI_Wtime();
 
@@ -186,26 +166,24 @@ int main(int argc, char *argv[]) {
 			if (myrank == 0) {
 
 				// Allocating memory for the whole matrix
-				allocate_init_2Dmatrix(&a, n, n);
+				allocate_root_matrix(&a, n, n);
 
 				// Master sends chuncks to every other node
 				for (i = 1; i < np; i++) {
-					int i_lower_index = get_lower_index(i, max_rows);
-					int i_upper_index = get_upper_index(i, max_rows, n);
-					int i_num_rows = get_node_rows(i_lower_index, i_upper_index);
-					int i_num_elems = get_node_elems(i_num_rows, n);
+					int i_offset = get_node_offset(i, n, max_rows);
+					int i_elems = get_node_elems(i, n, max_rows);
 
-					MPI_Send(&a[i_lower_index], i_num_elems, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+					MPI_Send(&a[i_offset], i_elems, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
 				}
 			}
 			else {
 
 				// Allocating the exact memory to the rows receiving
-				allocate_nodes_2Dmatrix(&a, num_rows, n);
+				allocate_node_matrix(&a, elems);
 				MPI_Status status;
 
 				// Receiving the data from the master node
-				MPI_Recv(&a, num_elems, MPI_FLOAT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+				MPI_Recv(&a, elems, MPI_FLOAT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 			}
 			break;
 		}
@@ -213,11 +191,11 @@ int main(int argc, char *argv[]) {
 		case 1: {
 			if (myrank == 0) {
 				// Allocating memory for the whole matrix
-				allocate_init_2Dmatrix(&a, n, n);		
+				allocate_root_matrix(&a, n, n);		
 			}
 			else {
 				// Allocating the exact memory to the rows receiving
-				allocate_nodes_2Dmatrix(&a, num_rows, n);
+				allocate_node_matrix(&a, elems);
 			}
 
 			// Collective communication for scattering the matrix
@@ -246,23 +224,19 @@ int main(int argc, char *argv[]) {
 
 				// Master sends chuncks to every other node
 				for (i = 1; i < np; i++) {
-					int i_lower_index = get_lower_index(i, max_rows) + 1;		// +1 to skip cortex values
-					int i_upper_index = get_upper_index(i, max_rows, n) - 1;	// -1 to skip cortex values
-					int i_num_rows = get_node_rows(i_lower_index, i_upper_index);
-					int i_num_elems = get_node_elems(i_num_rows, n);
+					int i_offset = get_node_offset(i, n, max_rows) + n; 	// +n to skip cortex values
+					int i_elems = get_node_elems(i, n, max_rows) - (2*n);	// -2n to skip cortex values
 
 					// Receiving the data from the slave nodes
-					MPI_Recv(&a[i_lower_index], i_num_elems, MPI_FLOAT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+					MPI_Recv(&a[i_offset], i_elems, MPI_FLOAT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 				}
 			}
 			else {
-				int solved_lower_index = 1;						// Start at 1 to skip cortex values
-				int solved_upper_index = num_rows-2;			// Reach num_rows-2 to skip cortex values
-				int solved_rows = get_node_rows(solved_lower_index, solved_upper_index);
-				int solved_elems = get_node_elems(solved_rows, n);
+				int solved_offset = n;										// Start at n to skip cortex values
+				int solved_elems = num_elems - (2*n);						// Reach num_elems-2n to skip cortex values
 
 				// Compute num_elems sin la corteza
-				MPI_Send(&a[solved_lower_index], solved_elems, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
+				MPI_Send(&a[solved_offset], solved_elems, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
 			}
 
 			break;
@@ -276,7 +250,7 @@ int main(int argc, char *argv[]) {
 			break;
 		}
 	}
-	
+
 	double tfcom2 = MPI_Wtime();
 
 	printf("Tiempo de comunicacion: %f", (tfcom1-tscom1) + (tfcom2-tscom2));
